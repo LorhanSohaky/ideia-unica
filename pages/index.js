d572df80e9db9ba06e4b91e33d58baea1a9874a3
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-11-30 20:50:06 -0300

    Test prefetch

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Link from 'next/link';
 function Home() {
     return (<div>
         <h1>Home</h1>
+        <Link href="/tempo">
+            <a>Acessar página Tempo</a>
+        </Link>
 
         <Link href="/sobre">
             <a>Acessar página Sobre</a>
